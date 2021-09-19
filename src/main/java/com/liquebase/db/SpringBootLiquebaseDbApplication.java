d0aaf5e7f99c357115f8849be66b9c16598ed9d7
jbozarov <tech@jbozarov.com>
2021-09-19 14:37:23 -0400

    Project initiated

---
+++ src/main/java/com/liquebase/db/SpringBootLiquebaseDbApplication.java
@@ -0,0 +1,13 @@
+package com.liquebase.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLiquebaseDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLiquebaseDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liquebase/db/SpringBootLiquebaseDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liquebase.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLiquebaseDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
