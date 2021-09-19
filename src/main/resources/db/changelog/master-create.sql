37740844e260aa8d45a16c4aee8e12a5064eb2e6
jbozarov <tech@jbozarov.com>
2021-09-19 14:55:06 -0400

    Created insert sql file

--- src/main/resources/db/changelog/master-create.sql
+++ src/main/resources/db/changelog/master-create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE books (
 book_id int NOT NULL,
 book_name varchar(50) NULL,
-book_description varchar(200) NULL, 
+book_description varchar(200) NULL,
 book_price int null,
-book
+book_isbn varchar(100) null
 );
